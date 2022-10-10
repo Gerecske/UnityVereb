6ad7056024a4f90d35b8b5e2abc62e28f3b53f03
Gerecske <szil.gergo9@gmail.com>
2022-10-10 20:24:31 +0200

    Kocsis
    Elkezdett kocsis

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Rigidbody2D backTire;
+    public Rigidbody2D frontTire;
+    public float speed = 20f;
+    private float movement;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        frontTire.AddTorque((60 * speed * Time.fixedDeltaTime) * -1);
+    }
+}
