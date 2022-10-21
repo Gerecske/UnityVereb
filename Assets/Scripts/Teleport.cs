2862eeb6a251e759d4e51b6d45fb7dbfd5f172aa
Gerecske <szil.gergo9@gmail.com>
2022-10-21 22:17:39 +0200

    k

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -17,6 +17,9 @@ public class Teleport : MonoBehaviour
     float time3 = 0f;
     public TextMeshProUGUI textM;
     public TextMeshProUGUI textMTimes;
+    public Rigidbody2D backTire;
+    public Rigidbody2D frontTire;
+    int level = 1;
 
     void Update()
     {
@@ -32,12 +35,16 @@ public class Teleport : MonoBehaviour
             car.transform.position = spawn2.position;
             time1 = currentTime;
             currentTime = 0f;
+            level = 2;
+            textMTimes.text = "Score\n" + "1: " + time1.ToString();
         }
         else if (collision.name == "flag2")
         {
             car.transform.position = spawn3.position;
             time2 = currentTime;
             currentTime = 0f;
+            level = 3;
+            textMTimes.text = "Score\n" + "1: " + time1.ToString() + "\n2: " + time2.ToString();
         }
         else if (collision.name == "flag3")
         {
@@ -45,11 +52,26 @@ public class Teleport : MonoBehaviour
             time3 = currentTime;
             currentTime = 0f;
             textMTimes.text = "Score\n" + "1: " + time1.ToString() + "\n2: " + time2.ToString() + "\n3: " + time3.ToString();
+            level = 1;
         }
     }
 
     public void Restart()
     {
-
+        if (level == 1)
+        {
+            car.transform.position = spawn1.position;
+            car.transform.rotation = spawn1.rotation;
+        }
+        else if (level == 2)
+        {
+            car.transform.position = spawn2.position;
+            car.transform.rotation = spawn2.rotation;
+        }
+        else if (level == 3)
+        {
+            car.transform.position = spawn3.position;
+            car.transform.rotation = spawn3.rotation;
+        }
     }
 }
